ee22368e4366fa20ec86fde163a7e83b630a4970
ANGRYCONE <theepicmanofficial@gmail.com>
2026-07-06 21:10:40 +1000

    Delete macos_cleaner.sh

--- macos_cleaner.sh
+++
@@ -1,6 +0,0 @@
-echo "HOLD UP! You downloaded the outdated script."
-echo "I always forget to update the source file here because it's way too much work."
-echo "Please grab the latest version from Releases instead: https://github.com/ANGRYCONE/Simple-macOS-System-Data-Cleaner/releases"
-echo ""
-echo "Thank you 👍"
-exit 1
